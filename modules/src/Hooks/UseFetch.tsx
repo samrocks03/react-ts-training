d16d93fc4dc9cebb7998324fd07fd5907b053ea0
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-03-07 10:05:35 +0530

    upd: package.json

--- modules/src/Hooks/UseFetch.tsx
+++ modules/src/Hooks/UseFetch.tsx
@@ -9,7 +9,7 @@ export const useFetch = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:5000/todossss");
+        const response = await fetch("http://localhost:5000/todos");
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
