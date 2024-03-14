0523e66e80e2ba22dd6862929ad2743f9faaeb0a
Samarth Kulkarni <svkulkarni23@gmail.com>
2024-03-14 16:13:52 +0530

    add: use Delete todo in todo.hooks.ts

--- modules/src/Hooks/todo.hooks.ts
+++ modules/src/Hooks/todo.hooks.ts
@@ -1,16 +1,26 @@
 //Hooks/todo.hooks.ts
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 // import { useEffect, useState } from "react";
 import { Todo } from "../Components/Home";
 import { API_ENDPOINT } from "../constants";
 
 export const useFetch = () => {
- const { data, error, isLoading, refetch } = useQuery({
+  const { data, error, isLoading, refetch } = useQuery({
     queryKey: ['todos'],
     queryFn: async () => await axios.get<Todo[]>(`${API_ENDPOINT}`)
   })
 
-  return {data, error, isLoading, refetch}
+  return { data, error, isLoading, refetch }
 };
+
+
+export const useDeleteTodo = () => {
+  const { mutate, isSuccess, isPending} = useMutation({
+    mutationFn: (id: string) => {
+      return axios.delete(`${API_ENDPOINT}/${id}`)
+    }
+  })
+  return { deleteTodo: mutate , isdeleteSuccess: isSuccess, isDeletePending: isPending }
+}
